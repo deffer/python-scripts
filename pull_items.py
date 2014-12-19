0f8ca60199cee2f71a44d4e193bd7a6e6a944a2c
deffer <irina.benediktovich@gmail.com>
2014-12-19 16:35:00 +1300

    Pull from wow and push to local elastic

--- pull_items.py
+++ pull_items.py
@@ -1,9 +1,36 @@
 import requests
+import json
+import time
 
 wowItemUrl='http://us.battle.net/api/wow/item/'
 
-for i in range(1, 5):
-	itemid=18800+i
-	r=requests.get(wowItemUrl + str(itemid))
-	print r.text
-	
+print str(int(time.time() * 1000))
+
+errorsCount = 0
+successCount = 0
+countBase = 500
+
+for i in range(1, 501):
+        itemid=countBase + i
+        lastRequestTime = time.time()
+        r = requests.get(wowItemUrl + str(itemid))
+        #{status: "nok",reason: "unable to get item information."}
+        #{status: "nok",reason: "When in doubt, blow it up. (page not found)"}
+        jsonResp = r.json()
+        if "status" in jsonResp and jsonResp["status"] == "nok":
+                #print "ERROR", jsonResp["reason"], "\n"
+                errorsCount += 1
+                if (errorsCount % 50) == 0:
+                        print "Errors - "+str(errorsCount)+"\n"
+        else:
+                #print r.text, "\n"
+                requests.post("http://localhost:9200/wow/item/"+str(itemid), data=r.text)
+                successCount += 1
+                if (successCount % 50)==0:
+                        print "fetched item "+str(itemid)+"\n"
+
+        toWait = time.time() - (lastRequestTime+0.1)
+        if toWait>0:
+                time.sleep(toWait)
+
+print "FINISH. Errors - "+str(errorsCount)+" Success - "+str(successCount)
