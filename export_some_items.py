a8aa98c8661eabee0c54b0239b92a074fe95aa07
deffer <irina.benediktovich@gmail.com>
2015-07-16 23:55:32 +0000

    Cross reference wow auction items

--- export_some_items.py
+++ export_some_items.py
@@ -1,4 +1,4 @@
-#import requests
+import requests
 import io
 import codecs
 import json
@@ -14,6 +14,7 @@ successCount = 0
 fname="./auctions.json"
 
 itemsCache = dict()
+items = dict()
 errorsCache = set()
 
 f = io.open(fname, encoding="utf-8")
@@ -37,10 +38,20 @@ for a in auctions:
     id = a["auc"]
     count += 1
     if item in itemsCache:
-        #print "item found "+str(item)+" count "+str(itemsCache[item])
         itemsCache[item] = itemsCache[item]+1
     else:
         itemsCache[item] = 1
+        r = requests.get(elasticItemUrl + str(item))
+        jsonResp = r.json()
+        if "found" in jsonResp and (jsonResp["found"]=="false" or jsonResp["found"]==False):
+            if item in errorsCache:
+                errorsCache[item] = errorsCache[item]+1
+            else:
+                errorsCache[item] = 1
+        else:
+            itemobject=jsonResp["_source"]
+            itemobject["itemid"]=item
+            items[item]=itemobject
 
     if (count % 1000)==0:
         print "Errors - "+str(len(errorsCache))+" Last id "+str(id)
@@ -48,10 +59,26 @@ for a in auctions:
 #    recodedf.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (a["auc"], a["item"], a["owner"], a["ownerRealm"], a["bid"], a["buyout"], a["quantity"], a["timeLeft"], a["seed"], a["context"]))
     recodedf.write("%s|%s|%s|%s|%s|%s|%s|%s|%s|%s\n" % (a["auc"], a["item"], a["owner"], a["ownerRealm"], a["bid"], a["buyout"], a["quantity"], a["timeLeft"], a["seed"], a["context"]))
     
-
 recodedf.close()
+
+
+tosave=[]
+tosavefew=[]
 for d in sorted(itemsCache.items(), key=lambda(k,v): v, reverse=True):
-    if d[1]>1:
+    if d[1]>100:
         print "Item "+ str(d[0])+": "+str(d[1])
+        tosavefew.append(items[d[0]])
+
+    tosave.append(items[d[0]])
+
+itemsf = codecs.open("items.json", "w", "utf-8")
+json.dump(tosave, itemsf)
+itemsf.close()
+
+itemsfew = codecs.open("itemsfew.json", "w", "utf-8")
+json.dump(tosavefew, itemsfew)
+itemsfew.close()
+
+
 
 print "Auctions - "+str(count)+", items - "+str(len(itemsCache))
